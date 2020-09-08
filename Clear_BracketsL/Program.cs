d14ef5f5b12880047c7e420d9653c93ec12c601a
Александр Ломтев <allomtiev@gmail.com>
2020-09-08 19:03:46 +0300

    Добавьте файлы проекта.

---
+++ Clear_BracketsL/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Clear_BracketsL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Brackets("(a*(b-c)..... )") ➞ true
+                
+                Brackets(")(a-b-45/7*(a-34))") ➞ false
+                
+                Brackets("sin(90...)+.............cos1)") ➞ false
+             */
+            Console.WriteLine(Brackets(""));
+        }
+        public static bool Brackets(string memo)
+        {
+            var first = CountBrackets(memo, '(');
+            var second = CountBrackets(memo, ')');
+            return first == second ? true : false;
+        }
+        public static int CountBrackets(string memo, char brakect)
+        {
+            int count = 0;
+            foreach (var charM in memo)
+            {
+                count += charM == brakect ? 1 : 0;
+            }
+            return count;
+        }
+    }
+}
